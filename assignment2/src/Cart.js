6f0a74a4b98fa81e86cc975bf8b6551f8dc3f9a9
kanelaws <lawsk21@gmail.com>
2023-04-08 18:41:44 -0500

    cart table

--- assignment2/src/Cart.js
+++ assignment2/src/Cart.js
@@ -17,9 +17,43 @@ const Cart = (props) => {
 //   ));
   
   return (
+    
     <div>
-      <div>Items in Cart :</div>
-      <div></div>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Item</th>
+            <th scope="col">Name</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Price</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <tr>
+            <th>1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+          <th>2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+          <th>3</th>
+            <td>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+        <tfoot class="table-group-divider">
+          <tr>
+            <th scope="col" colspan="3">Total</th>
+              <td>$1,000,000</td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
   );
 };
