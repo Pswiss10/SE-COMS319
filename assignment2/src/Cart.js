6deb2dacbeda90ae90125bf3abd99a6fc6b0d69f
kanelaws <lawsk21@gmail.com>
2023-04-10 10:41:05 -0500

    Finished checkout cart table

--- assignment2/src/Cart.js
+++ assignment2/src/Cart.js
@@ -7,52 +7,49 @@ const Cart = (props) => {
 
     const cartItems = props.cartItems;
     // rest of the component code
+    let uniqueItems = [];
+    let itemQuantities = [0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < cartItems.length; ++i) {
+      itemQuantities[cartItems[i].id]++;
+      if(itemQuantities[cartItems[i].id] < 2) {
+        uniqueItems.push(cartItems[i]);
+      }
+    }
 
-const cartItemsList = cartItems.map((el) => (
-    <div key={el.id}>
-      <img class="img-fluid" src={el.image} width={100} alt={el.description}/>
-      {el.title}
-      ${el.price}
-    </div>
+  const cartItemsList = uniqueItems.map((el) => (
+    <tr key={el.id}>
+      <th scope="row">{itemQuantities[el.id]}</th>
+        <td scope="col"><img class="img-fluid" src={el.image} width={100} alt={el.description}/></td>
+        <td scope="col">{el.title}</td>
+        <td scope="col">${el.price}</td>
+    </tr>
   ));
+
+  let totalPrice = 0;
+
+  for (let i = 0; i < cartItems.length; ++i) {
+    totalPrice += cartItems[i].price;
+  }
   
   return (
     
     <div>
-      <div>Items in Cart :</div>
-      <div>{cartItemsList}</div>
-      <table class="table table-hover">
+      <table class="table table-striped">
         <thead>
           <tr>
+            <th scope="col">Quantity</th>
             <th scope="col">Item</th>
             <th scope="col">Name</th>
-            <th scope="col">Quantity</th>
             <th scope="col">Price</th>
           </tr>
         </thead>
         <tbody class="table-group-divider">
-          <tr>
-            <th>1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-          <th>2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-          <th>3</th>
-            <td>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+            {cartItemsList}
         </tbody>
         <tfoot class="table-group-divider">
           <tr>
-            <th scope="col" colspan="3">Total</th>
-              <td>$1,000,000</td>
+            <th scope="col" colSpan="3">Total</th>
+              <td>${totalPrice}</td>
           </tr>
         </tfoot>
       </table>
