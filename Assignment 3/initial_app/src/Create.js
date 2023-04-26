21ae9d86d750f79c5964184eac814bf44b2fdba4
Peter Wissman <pjwiss10@gmail.com>
2023-04-26 12:15:21 -0500

    server.js

--- Assignment 3/initial_app/src/Create.js
+++ Assignment 3/initial_app/src/Create.js
@@ -4,10 +4,50 @@ import React, { useState, useEffect } from "react";
 
 const Create = (props) => {
 
+    const [formData, setFormData] = useState({
+        _id: "",
+        Title: "",
+        Price: "",
+        Description: "",
+        Category: "",
+        image: "",
+        Rating: {
+            stars: "",
+            count: ""
+        },
+      });
+    
+      const handleChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+      };
+    
+      const handleSubmit = (event) => {
+        props.handleCreateSubmit(formData);
+      };
+
 
 return (
 
     <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          id:
+          <input
+            type="text"
+            name="_id"
+            value={formData._id}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
         <button type="button" onClick={() => props.handleViewChange('Main')}>Return to Main</button> <br/>
 
     </div>
--- Assignment 3/initial_app/src/Main.js
+++ Assignment 3/initial_app/src/Main.js
@@ -8,7 +8,6 @@ const Main = (props) => {
     return (
 
         <div>
-            
             <button type="button" onClick={() => props.handleViewChange('Create')}>Create New</button> <br/>
             <button type="button" onClick={() => props.handleViewChange('Read')}>Read All</button> <br/>
             <button id="Update" type="button" onClick={() => props.handleViewChange('Update')}>Update ID</button>
