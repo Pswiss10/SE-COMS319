70fef975a30236324f7dd4b7a437e21ee2af848b
Peter Wissman <pjwiss10@gmail.com>
2023-04-29 15:16:02 -0500

    Created all pages, waiting for some sort of backend before I can properlly test

--- Assignment 3/initial_app/src/Main.js
+++ Assignment 3/initial_app/src/Main.js
@@ -10,10 +10,10 @@ const Main = (props) => {
         <div>
             <button type="button" onClick={() => props.handleViewChange('Create')}>Create New</button> <br/>
             <button type="button" onClick={() => props.handleViewChange('Read')}>Read All</button> <br/>
-            <button id="Update" type="button" onClick={() => props.handleViewChange('Update')}>Update ID</button>
-            <input for="Update" type="number"></input> <br/>
-            <button id="Delete" type="button" onClick={() => props.handleViewChange('Delete')}>Delete ID</button>
-            <input for="Delete" type="number"></input> <br/>
+            <button id="Update" type="button" onClick={() => props.handleViewChange('Update')}>Update ID</button> <br/>
+            
+            <button id="Delete" type="button" onClick={() => props.handleViewChange('Delete')}>Delete ID</button> <br/>
+            
         </div>
 
     )
