204952f4d288fe559e24421372eb97d5ab5ccb23
Peter Wissman <pjwiss10@gmail.com>
2023-04-30 20:55:31 -0500

    madsdjfbd

--- Assignment 3/initial_app/src/Read.js
+++ Assignment 3/initial_app/src/Read.js
@@ -20,10 +20,11 @@ const Read = (props) => {
         console.log(data);
         setItems(data);
         });
+    }
 
     const showAllItems = items.map((el) => (
         <div key={el._id}>
-        <img src={el.image} width={30} alt="yes" /> <br />
+        <img src={el.image} width={30} alt="yes"/> <br />
         Title: {el.title} <br />
         Category: {el.category} <br />
         Price: {el.price} <br />
@@ -33,17 +34,19 @@ const Read = (props) => {
 
 return (
     <div>
+    <button type="button" onClick={() => props.handleViewChange('Main')}>Return to Main</button> <br/>
     <h1>Catalog of Products</h1>
     <button onClick={() => getAllProducts()}>Show All products</button>
     <h1>Show all available Products.</h1>
     <hr></hr>
     {<div>Products {showAllItems}</div>}
     <hr></hr>
+    <button type="button" onClick={() => props.handleViewChange('Main')}>Return to Main</button> <br/>
     </div>
     );
 
 
 }
 
-}
+
 export default Read;
