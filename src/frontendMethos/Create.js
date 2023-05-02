e9df09e20e110d043e42444deacc907fa1b9535b
Peter Wissman <pjwiss10@gmail.com>
2023-05-02 12:19:05 -0500

    added the backend code as a folder, and added front end code in it's folder

---
+++ src/frontendMethos/Create.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+
+
+
+const Create = (props) => {
+
+  const [addNewProduct, setAddNewProduct] = useState({
+    _id: 0,
+    title: "",
+    price: 0.0,
+    description: "",
+    category: "",
+    image: "http://127.0.0.1:4000/images/",
+    rating: { rate: 0.0, count: 0 },
+    });
+    
+    function handleChange(evt) {
+      const value = evt.target.value;
+      if (evt.target.name === "_id") {
+        setAddNewProduct({ ...addNewProduct, _id: value });
+      } else if (evt.target.name === "title") {
+        setAddNewProduct({ ...addNewProduct, title: value });
+        } else if (evt.target.name === "price") {
+        setAddNewProduct({ ...addNewProduct, price: value });
+        } else if (evt.target.name === "description") {
+        setAddNewProduct({ ...addNewProduct, description: value });
+        } else if (evt.target.name === "category") {
+        setAddNewProduct({ ...addNewProduct, category: value });
+        } else if (evt.target.name === "image") {
+        const temp = value;
+        setAddNewProduct({ ...addNewProduct, image: temp });
+        } else if (evt.target.name === "rate") {
+        setAddNewProduct({ ...addNewProduct, rating: { rate: value } });
+        } else if (evt.target.name === "count") {
+        const temp = addNewProduct.rating.rate;
+        setAddNewProduct({
+        ...addNewProduct,
+        rating: { rate: temp, count: value },
+        });
+      }
+    }
+    
+      function handleOnSubmit(e) {
+        e.preventDefault();
+        console.log(e.target.value);
+        fetch("http://localhost:4000/insert", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(addNewProduct),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+        console.log("Post a new product completed");
+        console.log(data);
+        if (data) {
+        //const keys = Object.keys(data);
+        const value = Object.values(data);
+        alert(value);
+        }
+        });
+        }
+
+
+return (
+
+<div>
+  <div class="p-3">
+    <h3>Add a new product :</h3>
+    <form action="">
+      <input type="number" class="form-control" placeholder="id?" name="_id" value={addNewProduct._id} onChange={handleChange} /><br/>
+      <input type="text" class="form-control" placeholder="title?" name="title" value={addNewProduct.title} onChange={handleChange} /><br/>
+      <input type="number" class="form-control" placeholder="price?" name="price" value={addNewProduct.price} onChange={handleChange} /><br/>
+      <input type="text" class="form-control" placeholder="description?" name="description" value={addNewProduct.description} onChange={handleChange} /><br/>
+      <input type="text" class="form-control" placeholder="category?" name="category" value={addNewProduct.category} onChange={handleChange} /><br/>
+      <input type="text" class="form-control" placeholder="image?" name="image" value={addNewProduct.image} onChange={handleChange} /><br/>
+      <input type="number" class="form-control" placeholder="rate?" name="rate" value={addNewProduct.rating.rate} onChange={handleChange} /><br/>
+      <input type="number" class="form-control" placeholder="count?" name="count" value={addNewProduct.rating.count} onChange={handleChange} /><br/>
+      <button type="submit" class="btn btn-primary btn-lg" onClick={handleOnSubmit}>submit</button>
+    </form>
+    <button type="button" class="btn btn-primary btn-lg" onClick={() => props.handleViewChange('Main')}>Return to Main</button> <br/>
+  </div>
+
+
+    </div>
+
+)
+}
+export default Create;
---
+++ src/frontendMethos/Update.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+
+
+
+const Update = (props) => {
+
+    const [thisPrice, setPrice] = useState(0);
+    const [checked4, setChecked4] = useState(false);
+    const [index, setIndex] = useState(0);
+    const [product, setProduct] = useState([]);
+  
+ 
+    useEffect(() => {
+      getAllProducts();
+      }, [checked4]);
+  
+    function getAllProducts() {
+      fetch("http://localhost:4000/")
+      .then((response) => response.json())
+      .then((data) => {
+      console.log("Show Catalog of Products :");
+      console.log(data);
+      setProduct(data);
+      });
+    }
+    
+    function handleChange(evt){
+      setPrice(evt.target.value);
+      console.log(evt);
+      console.log(evt.target.value);
+    }
+
+    function getOneByOneProductNext() {
+      if (product.length > 0) {
+      if (index === product.length - 1) setIndex(0);
+      else setIndex(index + 1);
+      }
+      }
+  
+    function getOneByOneProductPrev() {
+      if (product.length > 0) {
+      if (index === 0) setIndex(product.length - 1);
+      else setIndex(index - 1);
+  
+      }
+      }
+  
+    function updateOneProduct(updateid) {
+      console.log("Current id : ", product[index].id, "and index", index, "and price: ", thisPrice)
+      console.log("Product to update :", updateid);
+      fetch("http://localhost:4000/update/", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        _id: Number(updateid), 
+        price: Number(thisPrice)
+       }),
+      })
+      .then((response) => response.json())
+      .then((data) => {
+      console.log("Update completed : ", updateid);
+      console.log(data);
+      if (data) {
+      //const keys = Object.keys(data);
+      alert("Update completed of Product with ID = " + updateid);
+      }
+      });
+      setChecked4(!checked4);
+      }
+  
+    return (
+      <div>
+        <div>
+          <h3>Delete one product:</h3>
+          <div>
+          <input type="checkbox" class="form-check-input" id="acceptdelete" name="acceptdelete" checked={checked4}
+          onChange={(e) => setChecked4(!checked4)} />
+
+          <button class="btn btn-primary" onClick={() => getOneByOneProductPrev()}>Prev</button>
+          <button class="btn btn-primary" onClick={() => getOneByOneProductNext()}>Next</button>
+          New Price: <input type="number" placeholder="$xx.xx" name="price" onChange={handleChange}></input>
+          <button class="btn btn-primary" onClick={() => updateOneProduct(product[index].id)}>Update</button>
+          </div>
+          {checked4 && (
+          <div key={product[index]._id}>
+          <img src={product[index].image} width={30} alt="yes"/> <br />
+          Id:{product[index].id} <br />
+          Title: {product[index].title} <br />
+          Category: {product[index].category} <br />
+          Price: {product[index].price} <br />
+          Rate :{product[index].rating.rate} and Count:
+          {product[index].rating.count} <br />
+          </div>
+          )}
+        </div>
+        <button type="button" class="btn btn-primary btn-lg" onClick={() => props.handleViewChange('Main')}>Return to Main</button> <br/>
+  
+      
+    </div>
+  
+  )
+  
+  
+            
+  }
+export default Update;
