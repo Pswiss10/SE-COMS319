6bfc19f6a99f49c5db326ada28c040a3bfba03e6
kanelaws <lawsk21@gmail.com>
2023-05-02 15:50:35 -0500

    Changed dataSchema to work with json

--- mern_backend/dataSchema.js
+++ mern_backend/dataSchema.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose')
 const ReactFormDataSchema = new mongoose.Schema({
     _id: {type: Number},
-    title: {type: String},
-    price: {type: Number},
-    description: {type: String},
-    category: {type: String},
-    image: {type: String},
-    rating: {
-        rate : {type: Number},
-        count : {type: Number}
-        }
-} , { collection: "fakestore_catalog" })
+    playerTitle: {type: String},
+    playerDescription: {type: String},
+    position: {type: String},
+    team: {type: String},
+    playerImage: {type: String},
+    shoes: [
+        {
+            shoeTitle: {type: String},
+            price: {type: Number},
+            shoeImage: {type: String},
+            rating: 
+            { 
+                rate: {type: Number}, 
+                count: {type: Number} },
+            count: {type: Number},
+            featured: {type: Number}
+          }
+    ]
+}, { collection: "shoestore_catalog" })
 
 const Product = mongoose.model('Product', ReactFormDataSchema)
 module.exports = Product
